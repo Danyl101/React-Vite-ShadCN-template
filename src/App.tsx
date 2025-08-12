1d741b46abc62f5a73ed446a26ff7d088f0d9148
Danyl S John <danyl.s.john@gmail.com>
2025-08-13 01:47:27 +0530

    Initial Full Upload

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react"
+
+export default function App() {
+  const [list, setList] = useState<string[]>([])
+  const [input, setInput] = useState<string>("")
+
+}
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      api: path.resolve(__dirname, 'src/api'),
+      components: path.resolve(__dirname, 'src/components'),
+      typescript_api: path.resolve(__dirname, 'src/typescript_api'),
+       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
